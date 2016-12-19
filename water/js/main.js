6e6fb58cf624b11b07e3c79b506010d86c4958b0
Alex Gintoft <gintoft.a.s@gmail.com>
2016-12-20 00:53:40 +0700

    water_calc

---
+++ water/js/main.js
@@ -0,0 +1,65 @@
+$(function () {
+    
+    
+
+var cost = {
+    
+    hot: 80.23,
+    cold: 15.34,
+    sliv: 12.54
+    
+}
+
+var total = {
+    
+    hot: 0,
+    cold: 0,
+    sliv: 0
+    
+}
+
+var hot = 0;
+var cold = 0;
+    
+
+
+    
+document.querySelector("#hot_more").onclick = function (e) {
+
+    hot += 1;
+    
+    e.currentTarget.innerHTML = hot + ' М<sup>3</sup>';
+    
+};
+    
+document.querySelector("#cold_more").onclick = function (e) {
+
+    cold += 1;
+    
+    e.currentTarget.innerHTML = cold + ' М<sup>3</sup>';
+    
+};
+
+$('#calc').click(function(){
+    
+    total.hot = cost.hot * hot;
+    total.cold = cost.cold * cold;
+    total.sliv = cost.sliv * (hot + cold);
+    
+    /*
+    alert('Горячая - ' + total.hot);
+    alert('Холодная - ' + cold);
+    alert('Слив - ' + (hot + cold));
+    */
+    var total_price = total.cold + total.hot + total.sliv;
+    
+    $('#place_hot').html((hot).toFixed(2) + ' М<sup>3</sup> * ' + (cost.hot).toFixed(2) + ' руб. = ' + (total.hot).toFixed(2) + ' руб.');
+    $('#place_cold').html((cold).toFixed(2) + ' М<sup>3</sup> * ' + (cost.cold).toFixed(2) + ' руб. = ' + (total.cold).toFixed(2) + ' руб.');
+    $('#place_sliv').html((hot).toFixed(2) + ' М<sup>3</sup> + ' + (cold).toFixed(2) + ' М<sup>3</sup> * ' + (cost.sliv).toFixed(2) + ' руб. = ' + (total.sliv).toFixed(2) + ' руб.');
+    $('#place_total').html((total.hot).toFixed(2) + ' руб. + ' + (total.cold).toFixed(2) + ' руб. + ' + (total.sliv).toFixed(2) + ' руб. = ' + (total_price).toFixed(2) + ' руб.');
+    
+    
+})
+    
+
+})
