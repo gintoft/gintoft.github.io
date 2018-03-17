667929a3c4f91e29b39362e56e7c949705acf229
Alex Gintoft <gintoft.a.s@gmail.com>
2018-03-17 20:29:42 +0700

    vy

---
+++ 20!8/js/main.js
@@ -0,0 +1,102 @@
+$(function () {
+
+	$("#place").hide();
+	
+	
+$('#radio110').click(function(){
+	
+	jQuery("#radio110").attr('checked', false);
+	jQuery("#radio111").attr('checked', false);
+	jQuery("#radio112").attr('checked', false);
+	jQuery("#radio113").attr('checked', false);
+	jQuery("#radio114").attr('checked', false);
+	jQuery("#radio115").attr('checked', false);
+	
+	jQuery("#radio109").attr('checked', true);
+	
+})
+
+$('#radio111').click(function(){
+	
+	jQuery("#radio110").attr('checked', false);
+	jQuery("#radio111").attr('checked', false);
+	jQuery("#radio112").attr('checked', false);
+	jQuery("#radio113").attr('checked', false);
+	jQuery("#radio114").attr('checked', false);
+	jQuery("#radio115").attr('checked', false);
+	
+	jQuery("#radio109").attr('checked', true);
+	
+})
+
+$('#radio112').click(function(){
+	
+	jQuery("#radio110").attr('checked', false);
+	jQuery("#radio111").attr('checked', false);
+	jQuery("#radio112").attr('checked', false);
+	jQuery("#radio113").attr('checked', false);
+	jQuery("#radio114").attr('checked', false);
+	jQuery("#radio115").attr('checked', false);
+	
+	jQuery("#radio109").attr('checked', true);
+	
+})
+
+$('#radio113').click(function(){
+	
+	jQuery("#radio110").attr('checked', false);
+	jQuery("#radio111").attr('checked', false);
+	jQuery("#radio112").attr('checked', false);
+	jQuery("#radio113").attr('checked', false);
+	jQuery("#radio114").attr('checked', false);
+	jQuery("#radio115").attr('checked', false);
+	
+	jQuery("#radio109").attr('checked', true);
+	
+})
+
+$('#radio114').click(function(){
+	
+	jQuery("#radio110").attr('checked', false);
+	jQuery("#radio111").attr('checked', false);
+	jQuery("#radio112").attr('checked', false);
+	jQuery("#radio113").attr('checked', false);
+	jQuery("#radio114").attr('checked', false);
+	jQuery("#radio115").attr('checked', false);
+	
+	jQuery("#radio109").attr('checked', true);
+	
+})
+
+$('#radio115').click(function(){
+	
+	jQuery("#radio110").attr('checked', false);
+	jQuery("#radio111").attr('checked', false);
+	jQuery("#radio112").attr('checked', false);
+	jQuery("#radio113").attr('checked', false);
+	jQuery("#radio114").attr('checked', false);
+	jQuery("#radio115").attr('checked', false);
+	
+	jQuery("#radio109").attr('checked', true);
+	
+})
+
+
+
+$('#golos').click(function(){
+	
+	jQuery("#radio110").attr('checked', false);
+	jQuery("#radio111").attr('checked', false);
+	jQuery("#radio112").attr('checked', false);
+	jQuery("#radio113").attr('checked', false);
+	jQuery("#radio114").attr('checked', false);
+	jQuery("#radio115").attr('checked', false);
+	
+	jQuery("#radio109").attr('checked', true);
+	
+	$("#place").show();
+
+})
+
+
+})
