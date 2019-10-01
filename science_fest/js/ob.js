3b0ca7eb20cebd0fe6a87874a022d58065fc7819
Alex Gintoft <gintoft.a.s@gmail.com>
2019-10-01 22:54:00 +0700

    sf
    Sf

---
+++ science_fest/js/ob.js
@@ -0,0 +1,15 @@
+$(function () {
+
+	$(".card").hide();
+	
+    
+$('#NSUEM').click(function(){ $(".NSUEM").show(); })
+$('#NSPU').click(function(){  $(".NSPU").show(); })
+$('#NGONB').click(function(){ $(".NGONB").show(); })
+    
+$('#16OCT').click(function(){ $(".16OCT").show();})
+$('#17OCT').click(function(){ $(".17OCT").show();})
+$('#11OCT').click(function(){ $(".11OCT").show();})
+
+
+})
---
+++ sf/js/main.js
@@ -0,0 +1,5 @@
+function hideNSUEM(){
+	
+	$('.NSUEM').hide();
+	
+}
