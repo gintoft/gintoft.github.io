2f885bff94c51705838e38ca8a6bf297ddc26e75
Alex Gintoft <gintoft.a.s@gmail.com>
2019-10-06 22:03:34 +0700

    Pop

---
+++ psp/js/ob.js
@@ -0,0 +1,18 @@
+$(function () {
+
+$('#1').click(function(){
+    
+    $("#1").removeClass("btn-light");
+    $('#1').addClass("btn-success");
+    
+    
+    $("#gbi").attr('style',  'background-color:lightgreen');
+    $("#mki").attr('style',  'background-color:lightgreen');
+    $("#dp").attr('style',  'background-color:lightgreen');
+    $("#mpr").attr('style',  'background-color:lightgreen');
+    $("#mep").attr('style',  'background-color:lightgreen');
+    
+})
+	
+
+})
