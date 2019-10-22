f86aabb34ee92635cb4290e4e4e5921bfe622109
Alex Gintoft <gintoft.a.s@gmail.com>
2019-10-22 18:15:05 +0700

    123

--- psp/js/ob.js
+++ psp/js/ob.js
@@ -1,31 +1,33 @@
 events = [
     
-{ event: 'Генерация бизнес-идей' , speaker: 'Д. Шабалин' , datetime: '12 октября, 12:00' , place: '5-109' , eventID: '1' , link: '#'},
-{ event: 'Основы маркетинга' , speaker: 'А. Толстикова' , datetime: '21 октября, 17:00' , place: '5-213' , eventID: '2' , link: 'https://docs.google.com/forms/d/e/1FAIpQLSe4cbI6-tVg2w9vajKfoWgfZQ_eVaNPVFNfndltooxc0XBCtA/viewform'},
-{ event: 'Основы формирования команды' , speaker: 'Д. Москвин' , datetime: '22 октября, 17:00' , place: '5-104' , eventID: '3' , link: 'https://docs.google.com/forms/d/1NAwZRXf6q0tajijAUTObyr1nClrofGkrBAJT0OHVMvc/edit'},
-{ event: 'Продукт как потенциальная технология' , speaker: 'А. Старостин , Н. Дранишников' , datetime: '22 октября, 19:00' , place: '5-104' , eventID: '4' , link: 'https://docs.google.com/forms/d/1vdGASIl1Mpr7ZLaDQc69CeIdsrx48-Hd3EFPWW3EFDI/edit'},
-{ event: 'Основы бюджетирования и привлечение ресурсов' , speaker: 'В.Китайцева' , datetime: '21 октября, 17:00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '5' , link: 'https://docs.google.com/forms/d/1vomuab0vIPq6VUQhaHEGgN7k8l6cpf3etJx2nZyeGlU/edit'},
-{ event: 'SMM-маркетинг' , speaker: 'А. Изотова' , datetime: '28 октября, 17:00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '6' , link: 'https://docs.google.com/forms/d/1gJGyOBTFepUO5P2jRM7Y0UogpqtpPDwPhw4mgawbz98/edit'},
-{ event: 'Подбор персонала, архитектура команды' , speaker: 'Ю. Масалова' , datetime: '28 октября, 17:00' , place: '5-213' , eventID: '7' , link: 'https://docs.google.com/forms/d/129BmoXfJcmt3TYnXkvIv8hlK8RM1nk6UXB5eYvbE72I/edit'},
-{ event: 'Инжиниринг как компетенция' , speaker: 'Наноцентр СИГМА.Новосибирск' , datetime: '19 октября, 10:00 -20:00' , place: 'Ломоносова, 56, Актовый зал' , eventID: '8' , link: 'https://docs.google.com/forms/d/1kuEeMS5Lq-LkCLT3ynvQdBXGq8iD4UFPWiRJ9nhKnkk/edit'},
-{ event: 'Экономика как калькуляция (бизнес-планирование) (отменено)' , speaker: 'Спикер уточняется' , datetime: '23 октября, 17:00' , place: '5-104' , eventID: '9' , link: '#'},
-{ event: 'Менеджмент как искусство' , speaker: 'Е. Десяткова' , datetime: '22 октября, 18:30' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '10' , link: 'https://docs.google.com/forms/d/1mpIUZ0ycKSt7xoAPbGHGWWiT4dOdTgObNYSpsy9ov20/edit'},
-{ event: 'Личный бренд и брендинг компании' , speaker: 'Д. Петров' , datetime: '30 октября, 17:00' , place: '5-511' , eventID: '11' , link: 'https://docs.google.com/forms/d/1YkCnGkeZ0TwLgl1_m5daLhKalMdvtTM4Oc5a-oyUQH8/edit'},
-{ event: 'Конфликтология и взаимоотношение с конкурентами' , speaker: 'Т. Зотова' , datetime: '29 октября, 18:00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '12' , link: 'https://docs.google.com/forms/d/1B8PcaWssCuzyWa9a-mfo9RTeY1dJ93VpRs2jtxvMCfY/edit'},
-{ event: 'Юридическое сопровождение бизнеса. Расчетный счет и банковское обслуживание' , speaker: 'Спикер уточняется' , datetime: '1 ноября, 16:00' , place: 'Конференц-зал Сбербанка (ул. Серебренниковская, 20)' , eventID: '13' , link: 'https://docs.google.com/forms/d/1b0R0UKXir03BmyD8fAVlj5a2rug7Sju6LmZRgcMY9wA/edit'},
-{ event: 'Миссия и идеология бизнеса' , speaker: 'А. Лазарев' , datetime: '29 октября, 19:00' , place: '5-104' , eventID: '14' , link: 'https://docs.google.com/forms/d/13xew0UnCQ6IUr1nB-XEGlE-1gbKDDFxp7Mcw9FgCLog/edit'},
-{ event: 'Медиа-взаимодействие, PR' , speaker: 'А. Веселов' , datetime: '5 ноября, 17: 00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '15' , link: 'https://docs.google.com/forms/d/1p36P6JPV3gjwWWyhHuB6SOGAyqR3yWsrF6CgMK1ER2o/edit'},
-{ event: 'Работа на развитие команды, корпоративная культура' , speaker: 'Д. Шабалин' , datetime: '30 октября, 17:00' , place: '5-518' , eventID: '16' , link: 'https://docs.google.com/forms/d/1Sk_MuL4wh8XgXCAe9o9YlB7wElkZUdn47-4_HIp26A0/edit'},
-{ event: 'Упаковка продукта и упаковка франшизы' , speaker: 'А. Болотова' , datetime: '5 ноября, 17:00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '17' , link: 'https://docs.google.com/forms/d/19eaeGzhX4rTXX7Jf1mj9CatrNgGEFqJKT2-cfDkbvbs/edit'},
-{ event: 'Финансовая модель (бизнес-моделирование)' , speaker: 'Д. Москвин' , datetime: '28 октября, 17:00' , place: '5-411' , eventID: '18' , link: 'https://docs.google.com/forms/d/18IbvCkNV2_eFA4ijaOluHVhbdpYxffmBArm4cokX-lc/edit'},
-{ event: 'Поиск новых рынков: стратегия голубого океана' , speaker: 'А. Васенев' , datetime: '6 ноября, 17:00' , place: '5-213' , eventID: '19' , link: 'https://docs.google.com/forms/d/1f_Q-jb0v_q5YvQNG8wsMizCuFHnNweJkSauEEpZPk8E/edit'},
-{ event: 'Поиск новых рынков: технологии маркетинга' , speaker: 'Д. Петров' , datetime: '6 ноября, 17:00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '20' , link: 'https://docs.google.com/forms/d/1TzqE_9jSds0eKGV7jp9kOLNv8eP1CcKF-5w3Nd3mGCo/edit'},
-{ event: 'Делегирование полномочий и отчуждение проекта («…продай компанию»)' , speaker: 'Спикер уточняется' , datetime: '19 октября, 10:00 -20:00' , place: 'Ломоносова, 56, Актовый зал' , eventID: '21' , link: 'https://docs.google.com/forms/d/1EyVPGEgapMm5U3wwQKi1JgrJBbLs6-guZvl94536LFo/edit'},
-{ event: 'Мультипликативный эффект продукта (удержание клиента) (отменено)' , speaker: 'Спикер уточняется' , datetime: '5 ноября, 19:00' , place: '5-213' , eventID: '22' , link: '#'},
-{ event: 'Упаковка проекта для инвестора' , speaker: 'П. Новгородов, Ю. Коложвари' , datetime: '30 октября, 17:00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '23' , link: 'https://docs.google.com/forms/d/1lwIuYVSx349--qK6x44X4CDy7SFaHjTQNKza7xHnq80/edit'},
-{ event: 'Социальное предпринимательство' , speaker: 'Е. Дубровин' , datetime: '2-3 ноября' , place: 'Точка кипения НГТУ' , eventID: '24' , link: 'https://docs.google.com/forms/d/1O20RPl2AbmKxlDbdqc_oH_QBAfn-Ge1Bw3PtQO45Vkg/edit'},
-{ event: 'Тимбилдинг как корпоратив' , speaker: 'А. Подилько' , datetime: '26 октября' , place: 'Выездная площадка («Звезда»)' , eventID: '25' , link: 'https://docs.google.com/forms/d/1lOzoBHIFhUq7vp6-BfDNSrZWo3IKqXeFsppiCrh48yI/edit'},
-{ event: 'Заявки на гранты' , speaker: 'Спикер уточняется' , datetime: 'Дата уточняется' , place: 'Аудитория уточняется' , eventID: '26' , link: 'https://docs.google.com/forms/d/1HSJut9vEVt_d4LRs8IZpihz4FFPyL5lh9FnMVsqybWs/edit'}
+{ tooltipText: 'Зарегистрироваться!', event: 'Генерация бизнес-идей' , speaker: 'Д. Шабалин' , datetime: '12 октября, 12:00' , place: '5-109' , eventID: '1' , link: '#'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Основы маркетинга' , speaker: 'А. Толстикова' , datetime: '21 октября, 17:00' , place: '5-213' , eventID: '2' , link: 'https://docs.google.com/forms/d/e/1FAIpQLSe4cbI6-tVg2w9vajKfoWgfZQ_eVaNPVFNfndltooxc0XBCtA/viewform'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Основы формирования команды' , speaker: 'Д. Москвин' , datetime: '22 октября, 17:00' , place: '5-104' , eventID: '3' , link: 'https://docs.google.com/forms/d/1NAwZRXf6q0tajijAUTObyr1nClrofGkrBAJT0OHVMvc/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Продукт как потенциальная технология' , speaker: 'А. Старостин , Н. Дранишников' , datetime: '22 октября, 19:00' , place: '5-104' , eventID: '4' , link: 'https://docs.google.com/forms/d/1vdGASIl1Mpr7ZLaDQc69CeIdsrx48-Hd3EFPWW3EFDI/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Основы бюджетирования и привлечение ресурсов' , speaker: 'В.Китайцева' , datetime: '21 октября, 17:00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '5' , link: 'https://docs.google.com/forms/d/1vomuab0vIPq6VUQhaHEGgN7k8l6cpf3etJx2nZyeGlU/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'SMM-маркетинг' , speaker: 'А. Изотова' , datetime: '28 октября, 17:00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '6' , link: 'https://docs.google.com/forms/d/1gJGyOBTFepUO5P2jRM7Y0UogpqtpPDwPhw4mgawbz98/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Подбор персонала, архитектура команды' , speaker: 'Ю. Масалова' , datetime: '28 октября, 17:00' , place: '5-213' , eventID: '7' , link: 'https://docs.google.com/forms/d/129BmoXfJcmt3TYnXkvIv8hlK8RM1nk6UXB5eYvbE72I/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Инжиниринг как компетенция' , speaker: 'Наноцентр СИГМА.Новосибирск' , datetime: '19 октября, 10:00 -20:00' , place: 'Ломоносова, 56, Актовый зал' , eventID: '8' , link: 'https://docs.google.com/forms/d/1kuEeMS5Lq-LkCLT3ynvQdBXGq8iD4UFPWiRJ9nhKnkk/edit'},
+{ tooltipText: 'Модуль отменен. Регистрация недоступна.', event: 'Экономика как калькуляция (бизнес-планирование)' , speaker: 'Спикер уточняется' , datetime: '23 октября, 17:00' , place: '5-104' , eventID: '9' , link: '#'},
+//{ tooltipText: 'Модуль отменен. Регистрация недоступна.', event: 'Экономика как калькуляция (бизнес-планирование)' , speaker: 'Спикер уточняется' , datetime: '23 октября, 17:00' , place: '5-104' , eventID: '9' , link: '#'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Менеджмент как искусство' , speaker: 'Е. Десяткова' , datetime: '22 октября, 18:30' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '10' , link: 'https://docs.google.com/forms/d/1mpIUZ0ycKSt7xoAPbGHGWWiT4dOdTgObNYSpsy9ov20/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Личный бренд и брендинг компании' , speaker: 'Д. Петров' , datetime: '30 октября, 17:00' , place: '5-511' , eventID: '11' , link: 'https://docs.google.com/forms/d/1YkCnGkeZ0TwLgl1_m5daLhKalMdvtTM4Oc5a-oyUQH8/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Конфликтология и взаимоотношение с конкурентами' , speaker: 'Т. Зотова' , datetime: '29 октября, 18:00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '12' , link: 'https://docs.google.com/forms/d/1B8PcaWssCuzyWa9a-mfo9RTeY1dJ93VpRs2jtxvMCfY/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Юридическое сопровождение бизнеса. Расчетный счет и банковское обслуживание' , speaker: 'Спикер уточняется' , datetime: '1 ноября, 16:00' , place: 'Конференц-зал Сбербанка (ул. Серебренниковская, 20)' , eventID: '13' , link: 'https://docs.google.com/forms/d/1b0R0UKXir03BmyD8fAVlj5a2rug7Sju6LmZRgcMY9wA/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Миссия и идеология бизнеса' , speaker: 'А. Лазарев' , datetime: '29 октября, 19:00' , place: '5-104' , eventID: '14' , link: 'https://docs.google.com/forms/d/13xew0UnCQ6IUr1nB-XEGlE-1gbKDDFxp7Mcw9FgCLog/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Медиа-взаимодействие, PR' , speaker: 'А. Веселов' , datetime: '5 ноября, 17: 00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '15' , link: 'https://docs.google.com/forms/d/1p36P6JPV3gjwWWyhHuB6SOGAyqR3yWsrF6CgMK1ER2o/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Работа на развитие команды, корпоративная культура' , speaker: 'Д. Шабалин' , datetime: '30 октября, 17:00' , place: '5-518' , eventID: '16' , link: 'https://docs.google.com/forms/d/1Sk_MuL4wh8XgXCAe9o9YlB7wElkZUdn47-4_HIp26A0/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Упаковка продукта и упаковка франшизы' , speaker: 'А. Болотова' , datetime: '5 ноября, 17:00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '17' , link: 'https://docs.google.com/forms/d/19eaeGzhX4rTXX7Jf1mj9CatrNgGEFqJKT2-cfDkbvbs/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Финансовая модель (бизнес-моделирование)' , speaker: 'Д. Москвин' , datetime: '28 октября, 17:00' , place: '5-411' , eventID: '18' , link: 'https://docs.google.com/forms/d/18IbvCkNV2_eFA4ijaOluHVhbdpYxffmBArm4cokX-lc/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Поиск новых рынков: стратегия голубого океана' , speaker: 'А. Васенев' , datetime: '6 ноября, 17:00' , place: '5-213' , eventID: '19' , link: 'https://docs.google.com/forms/d/1f_Q-jb0v_q5YvQNG8wsMizCuFHnNweJkSauEEpZPk8E/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Поиск новых рынков: технологии маркетинга' , speaker: 'Д. Петров' , datetime: '6 ноября, 17:00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '20' , link: 'https://docs.google.com/forms/d/1TzqE_9jSds0eKGV7jp9kOLNv8eP1CcKF-5w3Nd3mGCo/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Делегирование полномочий и отчуждение проекта («…продай компанию»)' , speaker: 'Спикер уточняется' , datetime: '19 октября, 10:00 -20:00' , place: 'Ломоносова, 56, Актовый зал' , eventID: '21' , link: 'https://docs.google.com/forms/d/1EyVPGEgapMm5U3wwQKi1JgrJBbLs6-guZvl94536LFo/edit'},
+{ tooltipText: 'Модуль отменен. Регистрация недоступна.', event: 'Мультипликативный эффект продукта (удержание клиента)' , speaker: 'Спикер уточняется' , datetime: '5 ноября, 19:00' , place: '5-213' , eventID: '22' , link: '#'},
+//{ tooltipText: 'Модуль отменен. Регистрация недоступна.', event: 'Мультипликативный эффект продукта (удержание клиента)' , speaker: 'Спикер уточняется' , datetime: '5 ноября, 19:00' , place: '5-213' , eventID: '22' , link: '#'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Упаковка проекта для инвестора' , speaker: 'П. Новгородов, Ю. Коложвари' , datetime: '30 октября, 17:00' , place: 'Бизнес-инкубатор НГУЭУ' , eventID: '23' , link: 'https://docs.google.com/forms/d/1lwIuYVSx349--qK6x44X4CDy7SFaHjTQNKza7xHnq80/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Социальное предпринимательство' , speaker: 'Е. Дубровин' , datetime: '2-3 ноября' , place: 'Точка кипения НГТУ' , eventID: '24' , link: 'https://docs.google.com/forms/d/1O20RPl2AbmKxlDbdqc_oH_QBAfn-Ge1Bw3PtQO45Vkg/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Тимбилдинг как корпоратив' , speaker: 'А. Подилько' , datetime: '26 октября' , place: 'Выездная площадка («Звезда»)' , eventID: '25' , link: 'https://docs.google.com/forms/d/1lOzoBHIFhUq7vp6-BfDNSrZWo3IKqXeFsppiCrh48yI/edit'},
+{ tooltipText: 'Зарегистрироваться!', event: 'Заявки на гранты' , speaker: 'Спикер уточняется' , datetime: 'Дата уточняется' , place: 'Аудитория уточняется' , eventID: '26' , link: 'https://docs.google.com/forms/d/1HSJut9vEVt_d4LRs8IZpihz4FFPyL5lh9FnMVsqybWs/edit'}
     
 ]
 
@@ -34,11 +36,13 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip();
     fillTable();
     
+    fillgray();
+    
 })
 
-function dravCell(event, speaker, datetime, place, enentID, link, currentCell) {
+function dravCell(event, speaker, datetime, place, enentID, link, currentCell, tooltipText) {
     
-    html = '<h6><a href="' + link + '" target="_blank" data-toggle="tooltip" data-placement="top" title="Зарегистрироваться!">' + event + '</a></h6>';
+    html = '<h6><a href="' + link + '" target="_blank" data-toggle="tooltip" data-placement="top" title="' + tooltipText + '">' + event + '</a></h6>';
     html += '<span class="badge badge-warning"><i class="fas fa-calendar-day"></i> ' + datetime + '</span>  ';
     html += '<span class="badge badge-success"><i class="fas fa-user"></i> ' + speaker + '</span>  <br>';
     html += '<span class="badge badge-light"><i class="fas fa-building"></i> ' + place + '</span>  <br>';
@@ -52,7 +56,19 @@ function fillTable(){
     for (i = 0; i < events.length; i++){
         
         currentCell = i + 1;
-        dravCell(events[i].event, events[i].speaker, events[i].datetime, events[i].place, events[i].eventID, events[i].link, '#' + currentCell);
+        dravCell(events[i].event, events[i].speaker, events[i].datetime, events[i].place, events[i].eventID, events[i].link, '#' + currentCell, events[i].tooltipText);
         
     }
+}
+
+function fillgray(){
+    
+    html = '<div><h6 style="color: gray">Модуль отменен</h6></div>';
+    
+    $('#9').detach();
+    $('#rmv9').append(html);
+    
+    $('#22').detach();
+    $('#rmv22').append(html);
+    
 }
